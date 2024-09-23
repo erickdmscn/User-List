72ad7b4a66766adcae99e5352fadf415b5fd3087
Erick Damasceno <ericksantoos46@hotmail.com>
2024-09-22 23:35:19 -0300

    Atualizando as rotas e messages

--- backend/src/controller.ts
+++ backend/src/controller.ts
@@ -6,29 +6,29 @@ export const createUser = async (req: Request, res: Response): Promise<Response>
   try {
     const { email, name } = req.body;
 
-    // Verificar se o e-mail é válido
+    // Verifica se o e-mail é válido
     if (!validator.isEmail(email)) {
-      return res.status(400).json({ message: 'E-mail inválido' });
+      return res.status(400).json({ message: 'E-mail inválido.' });
     }
 
-    // Verificar se o e-mail já está cadastrado
+    // Verifica se o e-mail já existe
     const existingUser = await UserModel.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ message: 'E-mail já existente' });
+      return res.status(400).json({ message: 'E-mail já existente.' });
     }
-
+    // Valida o nome
     const trimmedName = name.trim();
     if (!trimmedName || trimmedName.length < 3 || trimmedName.length > 50 || !/^[a-zA-Z\s'-]+$/.test(trimmedName)) {
-      return res.status(400).json({ message: 'Utilize um nome válido' });
+      return res.status(400).json({ message: 'Insira um nome válido.' });
     }
-    // Criar novo usuário
+    // Cria novo usuário
     const newUser = new UserModel({ email, name });
     await newUser.save();
 
-    return res.status(201).json({ message: 'Usuário criado com sucesso', newUser });
+    return res.status(201).json({ message: 'Usuário criado com sucesso.', newUser });
   } catch (error) {
     console.error('Erro ao criar usuário:', error);
-    return res.status(500).json({ message: 'Erro ao criar usuário', error });
+    return res.status(500).json({ message: 'Erro ao criar usuário.', error });
   }
 }
 
@@ -42,40 +42,6 @@ export const getUsers = async (_req: Request, res: Response): Promise<void> => {
   }
 }
 
-// Obter um usuário pelo ID
-export const getUserById = async (req: Request, res: Response): Promise<void> => {
-  try {
-    const user: IUser | null = await UserModel.findById(req.params.id)
-    if (!user) {
-      res.status(404).json({ message: 'Usuário não encontrado' })
-      return
-    }
-    res.status(200).json(user)
-  } catch (error) {
-    res.status(500).json({ message: 'Erro ao buscar usuário', error })
-  }
-}
-
-// Atualizar um usuário pelo ID
-export const updateUser = async (req: Request, res: Response): Promise<void> => {
-  try {
-    const { email, name } = req.body
-    const updatedUser: IUser | null = await UserModel.findByIdAndUpdate(
-      req.params.id,
-      { email, name },
-      { new: true }
-    )
-    if (!updatedUser) {
-      res.status(404).json({ message: 'Usuário não encontrado' })
-      return
-    }
-    res.status(200).json({ message: 'Usuário atualizado com sucesso', updatedUser })
-  } catch (error) {
-    res.status(500).json({ message: 'Erro ao atualizar usuário', error })
-  }
-}
-
-// Deletar um usuário pelo ID
 export const deleteUser = async (req: Request, res: Response): Promise<void> => {
   try {
     const deletedUser: IUser | null = await UserModel.findByIdAndDelete(req.params.id)
